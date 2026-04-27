92cbb712552633a05b9a75cda6a71c5e0fe5f374
Vishal <surthivishal1@gmail.com>
2026-04-27 22:37:30 +0530

    fix time import in result start

--- app.py
+++ app.py
@@ -1075,7 +1075,8 @@ def ou_result_start():
         if len(hall_ticket) != 12:
             return jsonify({'error': 'Invalid hall ticket'}), 400
         
-        job_id = hall_ticket + "" + str(int(_import_('time').time()))
+        import time
+        job_id = hall_ticket + "_" + str(int(time.time()))
         thread = threading.Thread(target=check_result_background, args=(job_id, hall_ticket))
         thread.daemon = True
         thread.start()
